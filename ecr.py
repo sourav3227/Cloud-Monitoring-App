1f2ecccd1e162f5edad33abb27c3623a59a6375f
sourav322 <125632100+sourav322@users.noreply.github.com>
2023-10-05 14:37:19 +0530

    added files

--- ecr.py
+++ ecr.py
@@ -1,11 +1,11 @@
 # boto3 - to create any resource in aws you need boto3
+# To create a ecr repo in aws 
 import boto3
-
 
 ecr_client = boto3.client('ecr', region_name='ap-south-1')
 
-repository_name = 'cloud-native-monitoring-app'
+repository_name = "my_monitoring_app_image"
 response = ecr_client.create_repository(repositoryName=repository_name)
 
-repository_uri = response['repository']['repositoryUri']
+repository_uri = response ['repository']['repositoryUri']
 print(repository_uri)
--- eks.py
+++ eks.py
@@ -1,28 +1,29 @@
+#create deployment and service
 from kubernetes import client, config
 
-#load kubernetes configuration
+# Load Kubernetes configuration
 config.load_kube_config()
 
-# create a kubernetes API Client
+# Create a Kubernetes API client
 api_client = client.ApiClient()
 
-# define deployment 
+# Define the deployment
 deployment = client.V1Deployment(
-    metadata=client.V1ObjectMeta(name="cloud-native-monitoring-app"),
+    metadata=client.V1ObjectMeta(name="my-flask-app"),
     spec=client.V1DeploymentSpec(
         replicas=1,
         selector=client.V1LabelSelector(
-            match_labels={"app": "cloud-native-monitoring-app"}
+            match_labels={"app": "my-flask-app"}
         ),
         template=client.V1PodTemplateSpec(
             metadata=client.V1ObjectMeta(
-                labels={"app": "cloud-native-monitoring-app"}
+                labels={"app": "my-flask-app"}
             ),
             spec=client.V1PodSpec(
                 containers=[
                     client.V1Container(
                         name="my-flask-container",
-                        image="660023948352.dkr.ecr.ap-south-1.amazonaws.com/cloud-native-monitoring-app:latest",
+                        image="660023948352.dkr.ecr.ap-south-1.amazonaws.com/my_monitoring_app_image:latest",
                         ports=[client.V1ContainerPort(container_port=5000)]
                     )
                 ]
@@ -42,7 +43,7 @@ api_instance.create_namespaced_deployment(
 service = client.V1Service(
     metadata=client.V1ObjectMeta(name="my-flask-service"),
     spec=client.V1ServiceSpec(
-        selector={"app": "cloud-native-monitoring-app"},
+        selector={"app": "my-flask-app"},
         ports=[client.V1ServicePort(port=5000)]
     )
 )
